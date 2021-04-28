895524005c2bde970d4d7e22ef367a6fbfca05cb
root <matthewcarrella9@gmail.com>
2021-04-27 21:19:53 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+</style>
+
+<template>
+  <div id="app">
+ 
+    <CompanyData/> <!-- Added New tag -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import CompanyData from './components/Comp' //import new component
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CompanyData //register new component
+  }
+}
+</script>
+
+
---
+++ src/components/Comp.vue
@@ -0,0 +1,70 @@
+<template>
+
+<div>
+<table class="table table-stripped mt-4">
+	<thread>
+		<tr>
+			<tr>
+				<th scope="col">name</th>
+				<th scope="col">email</th>
+			</tr>
+	</thread>
+	<tbody>
+		<tr v-for="input in directory.edges" :key="input.id">
+			<td>{{ input.node.username }}</td>
+			<td>{{ input.node.email }}</td>
+		</tr>
+	</tbody>
+</table>
+
+</div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default{
+  	name: 'CompanyData',
+    data(){
+      return {
+      	directory: []
+      }
+    },
+    async mounted () {
+    	try {
+    		var result = await axios({
+    			method: 'POST',
+    			url: 'http://noumenal.herokuapp.com/graphql/',
+    			data: {
+    				query: `
+    				{
+  users {
+    edges {
+      node {
+        id
+        username
+        email
+        isActive
+        archived
+        verified
+        secondaryEmail
+      }
+    }
+  }
+}
+					     
+					       
+					 
+					
+    				`
+    			}
+    		})
+    		this.directory = result.data.data.users
+    	} catch (error) {
+    		console.error(error)
+    	}
+    }
+  }
+</script>
+
+<style scoped>
+</style>
